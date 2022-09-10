85c2e5331f7f9b13cc8079ef2f5243cfda4f45ef
Wildafahera03 <101865160+Wildafahera03@users.noreply.github.com>
2022-09-10 19:47:24 +0700

    Add files via upload

---
+++ Praktikum 2/Soal4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Soal4
+{
+  public static void main(String args[]){
+    System.out.println("Deret Bilangan Genap");
+        for(int i=1;i<=20;i++){
+            if (i%2==0)
+            System.out.print(i+" ");
+    /*System.out.println("\nDeret Bilangan Ganjil");
+        for(int i=1;i<=20;i++){
+            if (i%2!=0)
+            System.out.print(i+" ");*/
+        }
+  }
+}
