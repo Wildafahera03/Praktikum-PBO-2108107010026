019173ecdef78e6107d2b8f38f2174551cc44017
Wildafahera03 <101865160+Wildafahera03@users.noreply.github.com>
2022-09-10 19:29:54 +0700

    Add files via upload

---
+++ Praktikum 2/Soal4.java
@@ -0,0 +1,12 @@
+public class Soal4
+{
+    public static void main(String arg[]){
+        for (int i=5; i>=0; i--)
+        {
+            for (int j=0; j<i; j++){
+            System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+} 
